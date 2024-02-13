aaaf1f950f19f962b618a7b3ed6bd542b0adab1b
Chai Wen Xuan <vicentchai@hotmail.com>
2024-02-13 19:37:43 +0800

    test

--- services/chatgptAnalyzer.py
+++ services/chatgptAnalyzer.py
@@ -4,8 +4,9 @@ from openai import OpenAI
 client = OpenAI()
 openai.api_key = os.environ.get('OPENAI_API_KEY')
 
+print("Test Print")
 print(os.environ.get('OPENAI_API_KEY'))
-print("testtest")
+
 # 2 types of data:
 # 1. Miro notes
 # 2. Essay
